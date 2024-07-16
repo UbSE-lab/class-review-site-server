cb483d5eee5b21a12dcada935f3c7ce907c7fcc6
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-07-16 09:11:57 +0900

    fix: ClassList data jpa 메소드 변경

--- src/main/java/org/classreviewsite/classlist/infrastructure/ClassListDataRepository.java
+++ src/main/java/org/classreviewsite/classlist/infrastructure/ClassListDataRepository.java
@@ -1,9 +1,26 @@
 package org.classreviewsite.classlist.infrastructure;
 
 import org.classreviewsite.data.ClassList;
+import org.classreviewsite.data.Lecture;
+import org.classreviewsite.data.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ClassListDataRepository extends JpaRepository<ClassList, Long> {
+
+
+    @Query("select m from ClassList m join fetch m.lecture")
+    List<ClassList> findByClassListWithProfessorName();
+
+
+
+    @Query("select m from ClassList m join fetch m.lecture where m.lecture.lectureId = :lectureId")
+    Optional<ClassList> findByLectureIdWithProfessorName(@Param("lectureId") Long lectureId);
+
 }
