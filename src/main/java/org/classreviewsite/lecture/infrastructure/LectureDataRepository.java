922008fe3c9c07e36400a8b47dc50c201200e425
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-07-16 09:22:57 +0900

    fix: 강의 data jpa 메소드 수정

--- src/main/java/org/classreviewsite/lecture/infrastructure/LectureDataRepository.java
+++ src/main/java/org/classreviewsite/lecture/infrastructure/LectureDataRepository.java
@@ -12,10 +12,16 @@ import java.util.Optional;
 @Repository
 public interface LectureDataRepository extends JpaRepository<Lecture, Long> {
 
+
     @Query("select distinct m.department from Lecture m")
     List<String> findDistinctDepartment();
 
 
     Optional<Lecture> findByLectureName(@Param("lectureName") String lectureName);
+
+
+    List<Lecture> findByUniversity(@Param("university") String university);
+
+
 
 }
