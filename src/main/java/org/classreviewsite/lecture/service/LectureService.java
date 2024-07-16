ddac6edbf12333df60761bccf47ecec267de3f79
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-07-16 09:23:20 +0900

    fix: 강의 service 로직 수정

--- src/main/java/org/classreviewsite/lecture/service/LectureService.java
+++ src/main/java/org/classreviewsite/lecture/service/LectureService.java
@@ -1,6 +1,7 @@
 package org.classreviewsite.lecture.service;
 
 import lombok.RequiredArgsConstructor;
+import org.classreviewsite.classlist.service.ClassListService;
 import org.classreviewsite.data.Lecture;
 import org.classreviewsite.auth.exception.LectureNotFoundException;
 import org.classreviewsite.lecture.infrastructure.LectureDataRepository;
@@ -15,11 +16,18 @@ public class LectureService {
 
     private final LectureDataRepository lectureDataRepository;
 
+    private final ClassListService classListService;
+
+    @Transactional(readOnly = true)
+    public List<Lecture> findAll(){
+        return lectureDataRepository.findAll();
+    }
 
     @Transactional(readOnly = true)
     public List<String> findDistinctDepartment(){
         return lectureDataRepository.findDistinctDepartment();
     }
+
 
 
 
@@ -29,10 +37,55 @@ public class LectureService {
     }
 
 
+    // 인기도
+    @Transactional(readOnly = true)
+    public Long countByLectureName(String lectureName){
+        Lecture lecture = lectureDataRepository.findByLectureName(lectureName).orElseThrow(() -> new LectureNotFoundException("존재하지 않는 강의입니다."));
+        return lecture.getReviewCount();
+    }
+
+
 
     @Transactional(readOnly = true)
     public Lecture findById(Long id){
         return lectureDataRepository.findById(id).orElseThrow(() -> new LectureNotFoundException("존재하지 않는 강의입니다."));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Lecture> findByUniversity(String university){
+        List<Lecture> list = lectureDataRepository.findByUniversity(university);
+        if(list.isEmpty()){
+            throw new LectureNotFoundException("해당 대학의 강의가 존재하지 않습니다.");
+        }
+        return list;
+    }
+
+    @Transactional
+    public void updateReviewCount(Lecture updateLecture){
+        updateLecture.updateReviewCount();
+    }
+
+    @Transactional
+    public void updateTotalStarLating(Lecture updateLecture, Long plusLating){
+        updateLecture.updateTotalStarLating(plusLating);
+    }
+
+    @Transactional
+    public void updateAverageStarLating(Lecture updateLecture){
+//        Long updateAverageLating = updateLecture.getAverageStarLating()/updateLecture.getReviewCount();
+        Lecture lecture = lectureDataRepository.findByLectureName(updateLecture.getLectureName()).orElseThrow(() -> new LectureNotFoundException("강의가 존재하지 않습니다."));
+        Long updateAverageLating = lecture.getAverageStarLating()/lecture.getReviewCount();
+        updateLecture.updateAverageStarLating(updateAverageLating);
+    }
+
+    @Transactional
+    public void cancelTotalStarLating(Lecture updateLecture, Long minusLating){
+        updateLecture.cancelTotalStarLating(minusLating);
+    }
+
+    @Transactional
+    public void cancelReviewCount(Lecture updateLecture){
+        updateLecture.cancelReviewCount();
     }
 
 }
